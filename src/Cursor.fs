40fea4f9f962e823cd5a753454642c9f28b98ce8
Stephan van Stekelenburg <stephan.van.stekelenburg@gmail.com>
2020-06-28 15:04:46 +0200

    dunno ?

--- src/Cursor.fs
+++ src/Cursor.fs
@@ -1,54 +1,81 @@
 ﻿namespace Fingle
 
-type ICursor =
-    interface end
+//type ICursor =
+//    interface end
 
-type IView =
-    interface end
+//type IView =
+//    interface end
 
-type Leaf =
-    {
-        FinalKey: Key
-    }
-    interface IView
+//type Leaf =
+//    {
+//        FinalKey: Key
+//    }
+//    interface IView
 
-type Branch =
-    {
-        head: IBranchTag
-        tail: ICursor
-    }
-    interface IView
+//type Branch =
+//    {
+//        head: IBranchTag
+//        tail: ICursor
+//    }
+//    interface IView
 
-type Cursor =
-    {
-        Keys: list<IBranchTag>
-        FinalKey: Key
-    }
-    interface ICursor
+//type Cursor =
+    //{
+    //    Keys: list<IBranchTag>
+    //    FinalKey: Key
+    //}
+    //interface ICursor
 
-    member __.Append(tag: Key -> IBranchTag, newFinalKey: Key) =
-        {
-            Keys = List.append __.Keys [tag(__.FinalKey)]
-            FinalKey = newFinalKey
-        }
+    //member __.Append(tag: Key -> IBranchTag, newFinalKey: Key) =
+    //    {
+    //        Keys = List.append __.Keys [tag(__.FinalKey)]
+    //        FinalKey = newFinalKey
+    //    }
 
-    member __.View(): IView =
-        match __.Keys with
-        | x :: xs ->
-            {
-                head = x
-                tail = { Keys = xs; FinalKey = __.FinalKey }
-            } :> IView
-        | _ ->
-            {
-                FinalKey = __.FinalKey
-            } :> IView
+    //member __.View(): IView =
+    //    match __.Keys with
+    //    | x :: xs ->
+    //        {
+    //            head = x
+    //            tail = { Keys = xs; FinalKey = __.FinalKey }
+    //        } :> IView
+    //    | _ ->
+    //        {
+    //            FinalKey = __.FinalKey
+    //        } :> IView
 
-    static member Doc() =
-        Cursor.WithFinalKey(DocK)
+    //static member Doc() =
+    //    Cursor.WithFinalKey(DocK)
 
-    static member WithFinalKey(finalKey: Key) =
-        {
-            Keys = List.empty
-            FinalKey = finalKey
-        }
+    //static member WithFinalKey(finalKey: Key) =
+        //{
+        //    Keys = List.empty
+        //    FinalKey = finalKey
+        //}
+//type View = interface end
+
+type Cursor(keys: BranchTag list, finalKey: Key) =
+    member Append(tag: Key): Cursor =
+
+
+//module Cursor =
+    type Cursor =
+        | Leaf of finalKey: Key
+        | Branch of head: BranchTag * tail: Cursor
+        //interface View
+
+
+
+        static member Doc() =
+            __.WithFinalKey(Key.DocK)
+
+        static member WithFinalKey(finalKey: Key): Cursor =
+            Cursor(List.empty, finalKey)
+
+
+
+//type Product (code:string, price:float) = 
+   //let isFree = price=0.0 
+   //new (code) = Product(code,0.0)
+   //member this.Code = code 
+   //member this.IsFree = isFree
